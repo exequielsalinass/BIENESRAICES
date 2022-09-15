28b2fd1cad295ffec7f652ee8abe51a05e1cb6b7
Exequiel <e.x.e_salinas3@outlook.com>
2022-09-14 22:00:22 -0300

    Instalado tailwindcss y creado el formulario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,16 +1,22 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-        
+        pagina: 'Iniciar Sesión'
     })
 }
 
 const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
-        
+        pagina: 'Crear Cuenta'
+    })
+}
+const formularioOlvidePassword = (req, res) => {
+    res.render('auth/olvide-password', {
+        pagina: 'Recupera tu Contraseña'
     })
 }
 
 export {
     formularioLogin,
-    formularioRegistro
+    formularioRegistro,
+    formularioOlvidePassword
 }

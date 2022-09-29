ac245d5efebbaa1afdf0f29241331557e48f7b3b
Exequiel <e.x.e_salinas3@outlook.com>
2022-09-29 18:16:37 -0300

    Creado el seeder

---
+++ seed/seeder.js
@@ -0,0 +1,55 @@
+import { exit } from "node:process";
+import categorias from "./categorias.js";
+import precios from "./precios.js";
+import Categoria from "../models/Categoria.js";
+import Precio from "../models/Precio.js";
+import db from "../config/db.js";
+
+const importarDatos = async () => {
+  try {
+    // Autenticar
+    await db.authenticate();
+
+    // Generar las columnas
+    await db.sync();
+
+    // Insertamos los datos
+    // Forma no muy profesional
+    /* await Categoria.bulkCreate(categorias)
+        await Precio.bulkCreate(precios) */
+    //* Forma profesional:
+    await Promise.all([
+      Categoria.bulkCreate(categorias),
+      Precio.bulkCreate(precios),
+    ]);
+    console.log("Datos importados correctamente");
+    exit();
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+const eliminarDatos = async () => {
+  try {
+    await Promise.all([
+      Categoria.destroy({ where: {}, truncate: true }),
+      Precio.destroy({ where: {}, truncate: true }),
+    ]);
+    console.log('Datos eliminados correctamente')
+    exit()
+    // Una forma mas fuerte de hacerlo
+    // await db.sync({force: true})
+  } catch (error) {
+    console.log(error);
+    exit(1);
+  }
+};
+
+if (process.argv[2] === "-i") {
+  importarDatos();
+}
+
+if (process.argv[2] === "-e") {
+  eliminarDatos();
+}

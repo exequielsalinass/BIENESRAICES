ceebae8466f50c00a7059e2106ef03a90b7fade8
Exequiel <e.x.e_salinas3@outlook.com>
2022-09-29 18:32:38 -0300

    Pasando valores de  categorias y precios al frontend

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,6 @@
+import Precio from '../models/Precio.js'
+import Categoria from '../models/Categoria.js'
+
 const admin = (req, res) => {
     res.render('propiedades/admin', {
         pagina: 'Mis Propiedades',
@@ -6,10 +9,18 @@ const admin = (req, res) => {
 }
 
 // Formulario para crear una nueva propiedad
-const crear = (req, res) => {
+const crear = async (req, res) => {
+    //* Consultar Modelo de Precio y Categorias
+    const [categorias, precios] = await Promise.all([
+        Categoria.findAll(),
+        Precio.findAll()
+    ])
+
     res.render('propiedades/crear', {
         pagina: 'Crear Propiedad',
-        barra: true
+        barra: true,
+        categorias,
+        precios
     })
 }
 
